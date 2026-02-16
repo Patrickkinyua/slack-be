da7b1496aed085ce324b53f1d5d004a76a3bdc04
patrick_mugo <patrickkinyua2004@gmail.com>
2026-02-16 21:13:50 +0300

    initial commit

---
+++ src/server.js
@@ -0,0 +1,16 @@
+import express from "express"
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const PORT =    process.env.PORT || 5001
+
+const app = express()
+
+app.get('/', (req,res) => {
+    res.send('hello world')
+})
+
+
+
+app.listen(PORT, ()=> console.log ("server is running on port 5001"))
