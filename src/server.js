0ae7cb03864843892b3f285ee8a23a61557d6cad
patrick_mugo <patrickkinyua2004@gmail.com>
2026-02-17 21:38:08 +0300

    fix typo

--- src/server.js
+++ src/server.js
@@ -16,7 +16,7 @@ const app = express()
 app.use(clerkMiddleware())
 app.use(express.json())
 
-app.use ("/api/ingest", serve({client:inngest,functions}))
+app.use ("/api/inngest", serve({client:inngest,functions}))
 
 
 app.get('/', (req,res) => {
